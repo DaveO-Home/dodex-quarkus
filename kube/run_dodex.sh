d7e5688bbe1707f22cf179f5188d7bbb3a33c89b
Dave O <dohitch88@gmail.com>
2024-06-19 18:06:04 -0700

    Fixed Docker Configuration

--- kube/run_dodex.sh
+++ kube/run_dodex.sh
@@ -2,4 +2,4 @@
 
 nohup /usr/bin/envoy --enable-fine-grain-logging --base-id 2  -l info -c /etc/envoy/envoy.yaml &
 
-${JAVA_HOME}/bin/java -jar /home/dodex/quarkus/dodex-quarkus-3.2.0-runner.jar
+${JAVA_HOME}/bin/java -jar /home/dodex/quarkus/dodex-quarkus-3.11.2-runner.jar
